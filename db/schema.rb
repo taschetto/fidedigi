f32f6b6ae370c4770308d63c8950f58de2785da0
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-13 11:58:38 -0300

    Add 'devise' gem and default configuration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150513145819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "ratio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "points", force: :cascade do |t|
+    t.integer  "company_id",                                        null: false
+    t.decimal  "balance",    precision: 12, scale: 2, default: 0.0
+    t.datetime "expiration"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+  end
+
+  add_index "points", ["company_id"], name: "index_points_on_company_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vouchers", force: :cascade do |t|
+    t.string   "code",                                                    null: false
+    t.integer  "company_id",                                              null: false
+    t.datetime "expiration"
+    t.decimal  "monetary_value", precision: 12, scale: 2, default: 0.0
+    t.integer  "points",                                  default: 0
+    t.boolean  "redeemed",                                default: false
+    t.datetime "created_at",                                              null: false
+    t.datetime "updated_at",                                              null: false
+  end
+
+  add_index "vouchers", ["company_id"], name: "index_vouchers_on_company_id", using: :btree
+
+  add_foreign_key "points", "companies"
+  add_foreign_key "vouchers", "companies"
 end
