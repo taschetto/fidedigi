56aa6f65b60cacf198161700bdda129945e2f981
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-17 17:04:37 -0300

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :managers
   devise_for :users
-  devise_for :clerks, controllers: { registrations: "clerks/registrations" }
+  devise_for :managers, controllers: { registrations: "managers/registrations" }
   root 'dashboard#index'
+
+  namespace :managers do
+    resource :company do
+    end
+  end
 end
