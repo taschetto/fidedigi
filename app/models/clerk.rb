c629689a3d22007badcc1641f1cb6bfd277022ad
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-18 18:58:49 -0300

    Update token generation for using a specific charset.

--- app/models/clerk.rb
+++ app/models/clerk.rb
@@ -1,6 +1,4 @@
 class Clerk < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -6,9 +6,10 @@ class Voucher < ActiveRecord::Base
   belongs_to :point
 
   def generate_token
+    charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
     self.code = loop do
-      random_token = SecureRandom.urlsafe_base64(16)
-      break random_token unless self.class.exists?(code: random_token)
+      random_token = (0...16).map{ charset.to_a[rand(charset.size)] }.join
+      break random_token unless self.class.exists?(code: random_token, redeemed: false)
     end
   end
 end
