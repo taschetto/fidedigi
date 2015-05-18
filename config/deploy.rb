3bcba9ee4bfd635961d0a42aafbc1b835a920f10
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-17 23:14:19 -0300

    Update seeds.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,6 @@ task :deploy => :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:db_seed'
     invoke :'rails:assets_precompile'
 
     to :launch do
