cc04807ec3b2a8f7a335de02c7016c63769afd9a
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-24 17:18:36 -0300

    Gol do Grêmio

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-if Company.any? then
+if !Company.any? then
 
   # Company
   manager = Manager.create email: Faker::Internet.safe_email,
